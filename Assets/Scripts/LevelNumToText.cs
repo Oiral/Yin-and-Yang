cafea5470b9e198c4223bf80066519f80cef964b
Trig Desktop <hamish@trig.nz>
2020-10-28 16:12:35 +1300

    Updated fonts

---
+++ Assets/Scripts/LevelNumToText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelNumToText : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+
+        int levelNum = SceneManager.GetActiveScene().buildIndex;
+
+        GetComponent<Text>().text = levelNum.ToString();
+    }
+}
