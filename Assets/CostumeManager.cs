76ca720837d7f5b47c68f888925391dcf674dc98
Hamish <hamish@pageland.co.nz>
2020-11-02 22:09:59 +1300

    Added in starts for player skins

---
+++ Assets/CostumeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CostumeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ChangePlayerMaterial(playerColours["Blue"]);
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public Dictionary<string, Material> playerColours = new Dictionary<string, Material>();
+
+    public void ChangePlayerMaterial(Material mat)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+            return;
+
+        MeshRenderer renderer = player.GetComponentInChildren<MeshRenderer>();
+
+        if (renderer == null)
+            return;
+
+        renderer.sharedMaterial = mat;
+    }
+
+}
---
+++ Assets/Editor/CostumeManagerEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CostumeManager))]
+public class CostumeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Show the default item
+        base.OnInspectorGUI();
+
+        CostumeManager manager = (CostumeManager)target;
+
+        Dictionary<string, Material> playerColours = new Dictionary<string, Material>();
+
+        foreach (var costumeMaterial in manager.playerColours)
+        {
+            GUILayout.BeginHorizontal();
+
+            string materialName = GUILayout.TextField(costumeMaterial.Key);
+
+            Material materialMat = (Material)EditorGUILayout.ObjectField(costumeMaterial.Value, typeof(Material), GUILayout.MinWidth(100f), GUILayout.MaxWidth(200f));
+
+            playerColours.Add(materialName, materialMat);
+
+            if (GUILayout.Button("-", GUILayout.Width(20f)))
+            {
+                playerColours.Remove(materialName);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("+"))
+        {
+            playerColours.Add((playerColours.Count + 1).ToString(), null);
+        }
+
+        manager.playerColours = playerColours;
+    }
+
+}
