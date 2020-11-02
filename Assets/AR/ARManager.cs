6890bd42177f71b27187136e2c52ecfc6eb7b746
Trig Desktop <hamish@trig.nz>
2020-11-02 16:35:25 +1300

    Trying to adjust the level to match the AR

---
+++ Assets/AR/ARManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARManager : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask mask;
+
+    public void MoveAR()
+    {
+        //Raycast from camera
+        RaycastHit hit;
+
+        Ray forwardRay = new Ray(cam.transform.position, cam.transform.forward);
+
+        if (Physics.Raycast(forwardRay, out hit, 20, mask)) 
+        {
+            transform.position = Vector3.zero - hit.point;
+        }
+    }
+}
