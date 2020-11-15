e597ed521d07fd715a56e235a6d87885d3bbf812
Hamish <hamish@pageland.co.nz>
2020-11-15 18:00:08 +1300

    Updated all scenes with the new models

---
+++ Assets/Editor/UpdateBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class UpdateBoard
+{
+    [MenuItem("Custom/Update Board %#g")]
+    public static void UpdateTheBoard()
+    {
+        GameObject.FindGameObjectWithTag("Board").GetComponent<BoardManager>().UpdateBoard();
+    }
+
+    [MenuItem("Custom/Revert Mesh In Tiles")]
+    public static void RevertMeshInTiles()
+    {
+        foreach (var tile in GameObject.FindObjectsOfType<TileScript>())
+        {
+            PrefabUtility.RevertObjectOverride(tile.GetComponentInChildren<MeshFilter>(), InteractionMode.UserAction);
+        }
+    }
+
+}
